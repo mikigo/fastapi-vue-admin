72c9c808933d81c45d8296a775811666f7b39637
mikigo <huangmingqiang@uniontech.com>
2023-04-12 18:42:08 +0800

    add api

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
-from typing import Union
+from typing import Union, Any
 
 import uvicorn
+from fastapi import FastAPI, Query, Path, Body, Cookie, Response, Header, status, Form
+from fastapi import File, UploadFile
 from pydantic import BaseModel, Field
-from fastapi import FastAPI, Query, Path, Body
 
 app = FastAPI()
 
@@ -71,11 +72,13 @@ async def body_test(
         results.update({"age": age})
     return {"results": results}
 
+
 class FieldItem(BaseModel):
     name: str
     description: Union[str, None] = Field(
         default=None, title="deepin", max_length=300
     )
+
 
 @app.post("/field/")
 async def field_test(
@@ -86,6 +89,47 @@ async def field_test(
     if age:
         results.update({"age": age})
     return {"field": results}
+
+
+@app.post("/cookieset")
+async def cookie_set(response: Response):
+    response.set_cookie(key="cookie1", value="mikigocookie11111")
+    return {"cookie_id": "ok"}
+
+
+@app.get("/cookieget")
+async def cookie_get(
+        ads_id: Union[str, None] = Cookie(default=None)
+):
+    return {"ads_id": ads_id}
+
+
+@app.get("/header")
+async def header_test(user_agent: Union[str, None] = Header(default=None)):
+    return {"User-Agent": user_agent}
+
+
+@app.post("/rsp_model", response_model=Item)
+async def response_model_test(item: Item) -> Any:
+    return item
+
+@app.post("/status", status_code=status.HTTP_201_CREATED)
+async def status_code_test(name: str):
+    return {"name": name}
+
+@app.post("/login")
+async def login(username: str = Form(), password: str = Form()):
+    return {"username": username}
+
+
+@app.post("/files/")
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
 
 
 if __name__ == '__main__':
