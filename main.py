53a13f526f623c417109a5e81c6f1c37cd167536
mikigo <huangmingqiang@uniontech.com>
2023-04-17 19:04:15 +0800

    add api

--- main.py
+++ main.py
@@ -163,6 +163,35 @@ async def auth_test(token: str = Depends(oauth2_scheme)):
     return {"token": token}
 
 
+import time
+
+from fastapi import Request
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 if __name__ == '__main__':
     uvicorn.run(
         app="main:app",
